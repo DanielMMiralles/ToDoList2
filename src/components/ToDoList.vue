d4a90fa43fb9a5f1c4f7a38fd8935b0ff5101b00
Daniel <damm172004@gmail.com>
2025-01-11 18:13:44 -0400

    Reactivity done

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,20 +1,38 @@
 <script setup lang="ts">
+    import { ref } from 'vue';
+    import type { Ref } from 'vue';
     import ListItem from './ListItem.vue';
+    
     type Item = {
         title: string;
         checked?: boolean;
     };
-    const GroupsItems: Item[] = [
+    const GroupsItems: Ref<Item[]> = ref([
         { title: 'Leer un libro al mes'},
         { title: 'Hacer ejercicio 3 veces por semana'},
         { title: 'Aprender algo nuevo cada día'},
-    ];
+    ]);
+
+    const updateItem = (item:Item): void => {
+        const updatedItem = findItemList(item);
+        if(updatedItem){
+            toggleItemCheck(updatedItem);
+        };
+    };
+
+    const findItemList = (item : Item): Item | undefined => {
+        return GroupsItems.value.find((groupItem) => groupItem.title === item.title);
+    };
+
+    const toggleItemCheck = (item: Item): void => {
+        item.checked = !item.checked;
+    };
 </script>
 
 <template>
     <ul >
         <li :key="index" v-for="(item, index) in GroupsItems">
-            <ListItem :isChecked="item.checked">
+            <ListItem :isChecked="item.checked" @updateItem="() => updateItem(item)">
                 {{ item.title }}
             </ListItem>
         </li>
